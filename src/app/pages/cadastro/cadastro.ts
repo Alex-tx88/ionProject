bd16b33fcf647b9349e2f0d26916e87329ca8f5f
Alex-tx88 <alexteix230@gmail.com>
2026-07-08 14:42:07 -0300

    atualização 2

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // <-- Importamos o módulo de formulários
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, FormsModule], // <-- Adicionamos no imports
   templateUrl: './cadastro.html',
-  styleUrl: './cadastro.css',
+  styleUrls: ['./cadastro.css']
 })
 export class Cadastro {
+  passoAtual: number = 1;
 
+  
+  dados = {
+    nome: '',
+    email: '',
+    telefone: '',
+    cpf: '',
+    senha: '',
+    confirmarSenha: ''
+  };
+
+  veiculoSelecionado: string = '';
+  lgpdAceito: boolean = false;
+
+  constructor(private router: Router) {}
+
+  get passo1Valido(): boolean {
+    return !!(this.dados.nome && this.dados.email && this.dados.telefone && this.dados.cpf);
+  }
+
+  get passo2Valido(): boolean {
+    return !!(this.dados.senha && this.dados.confirmarSenha && this.veiculoSelecionado && this.lgpdAceito);
+  }
+
+  proximoPasso() {
+    if (this.passo1Valido) {
+      this.passoAtual = 2;
+    }
+  }
+
+  passoAnterior() {
+    if (this.passoAtual === 2) {
+      this.passoAtual = 1;
+    }
+  }
+
+  selecionarVeiculo(tipo: string) {
+    this.veiculoSelecionado = tipo;
+  }
+
+  finalizarCadastro() {
+    if (this.passo2Valido) {
+      this.router.navigate(['/dashboard']);
+    }
+  }
 }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  lgpdAceito:boolean= false;
+  
+  constructor(private router: Router) {}
+
+  entrar() {
+    if(this.lgpdAceito){
+    this.router.navigate(['/dashboard']);
+    }
+  }
+
 
 }
